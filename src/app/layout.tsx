6312b73ade8a87aa43e8bfc7c299a1a32d3110ef
robertsthomas <robertsthomasd@gmail.com>
2025-05-27 21:30:56 -0400

    feat: integrate Google Tag Manager for enhanced tracking and analytics; add script and noscript tags in layout component for improved user engagement insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { PostHogProvider } from '~/components/PostHogProvider';
 import { QueryProvider } from '~/components/QueryProvider';
 import { LocationProvider } from '~/contexts/LocationContext';
 import { ConvexClientProvider } from '~/providers';
+import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({
@@ -86,7 +87,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full bg-gray-50">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N9MVPDTD');
+          `}
+        </Script>
+      </head>
       <body className={`h-full ${inter.variable} ${montserrat.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N9MVPDTD"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ConvexClientProvider>
           <PostHogProvider>
             <QueryProvider>
